54707eff896f9dd556f027ad2700c901a0a772d3
Heesuya <goqkddnf@naver.com>
2024-09-13 10:06:19 +0900

    오류 수정

--- 07_springboot_webproject/src/main/java/kr/co/iei/admin/model/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.iei.admin.model.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminService {
-	@Autowired
-}
